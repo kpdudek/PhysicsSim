9ffa2e5f70b0e3c275b08b3a63e1a35ade918077
Kurt <kpdudek2@gmail.com>
2021-10-24 17:55:15 -0400

    Switching computers. Forgot to save changes.

--- lib/CollisionChecker.c
+++ lib/CollisionChecker.c
@@ -3,10 +3,22 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
 double pixels_to_meters(double pixels){
     return 0.01 * pixels;
 }
 
 double meters_to_pixels(double meters){
     return meters / 0.01;
+}
+
+double circle_circle(double pose_1 [2][1], double radius_1, double pose_2 [2][1], double radius_2){
+    int result = 0;
+    return result;
+}
+
+double circle_mesh(){
+    int result = 0;
+    return result;
 }
